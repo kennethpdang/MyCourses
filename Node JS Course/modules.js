036e6c157958021ba6014b0a662012b520dcc743
Kenneth Dang <kennethpdang@gmail.com>
2024-12-19 16:53:16 -0600

    Updated lesson1.

---
+++ Node JS Course/modules.js
@@ -0,0 +1,11 @@
+//---------------------------------//
+/* Get the data from people.js     */
+//---------------------------------//
+// const xyz = require('./people');
+// console.log(xyz);
+
+//---------------------------------//
+/* An example of destructuring     */
+//---------------------------------//
+const { people, ages } = require('./people');
+console.log(people, ages);
---
+++ Node JS Course/people.js
@@ -0,0 +1,12 @@
+const people = ['yoshi', 'ryu', 'chun-li', 'mario'];
+const ages = [20, 25, 30, 35];
+
+console.log(people);
+
+//---------------------------------//
+/* This is picked up in modules.js */
+//---------------------------------//
+module.exports = {
+    people,
+    ages
+};
