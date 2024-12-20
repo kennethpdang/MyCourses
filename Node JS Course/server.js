6847e06db4bbfeacd80b6e3dab53e0625af13ce4
Kenneth Dang <kennethpdang@gmail.com>
2024-12-19 22:20:52 -0600

    Added index pages.

--- Node JS Course/server.js
+++ Node JS Course/server.js
@@ -1,7 +1,18 @@
 const http = require('http'); // To create servers.
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log('Request made.');
+
+    // Set header content type:
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.write();
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3000, 'localhost', () => {
