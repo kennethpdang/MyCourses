a9b241265a9a215e3e76610ffb4f862e4ac694a8
kennethpdang <kennethpdang@gmail.com>
2024-12-25 19:55:40 -0600

    Added functionality to connect to MongoDB

--- MERN Stack/backend/routes/workout.js
+++ MERN Stack/backend/routes/workout.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const router = express.Router();
-router.get();
+
+// Routes
+router.get('/', (request, response) => {
+    response.json({mssg: "Get all workouts."});
+});
+
+router.get('/:id', (request, response) => {
+    response.json({mssg: "Get a single workout."});
+});
+
+router.post('/', (request, response) => {
+    response.json({mssg: "Post a new workout."})
+});
+
+router.delete('/:id', (request, response) => {
+     response.json({mssg: "Delete a workout."});
+});
+
+router.patch('/:id', (request, response) => {
+    response.json({mssg: "Update a workout."});
+});
+
+module.exports = router;
--- MERN Stack/backend/server.js
+++ MERN Stack/backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const app = express();
 require('dotenv').config();
+const app = express();
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workout.js');
+
+app.use(express.json()); 
 
 app.use((request, response, next) => {
     console.log(request.path, request.method);
@@ -8,9 +12,10 @@ app.use((request, response, next) => {
 });
 
 // Routes
-app.get('/', (request, response) => {
-    response.json({mssg: "Welcome to the application."});
-});
+app.use('/api/workouts', workoutRoutes); // Uses all routes from workout routes.
+
+// Connect to MongoDB:
+mongoose.connect(process.env.MONGODB_URI)
 
 // Listen for Requests:
 app.listen(process.env.PORT, () => {
